4e145401ab9b2261a58ec75de48efc75c7ce3872
svogeti <sameervogeti@gmail.com>
2020-06-01 00:43:18 +0530

    Added API Call logic

--- cool_adapter.py
+++ cool_adapter.py
@@ -1,52 +1,53 @@
 from chatterbot.logic import LogicAdapter
-
+import spacy
 
 class MyLogicAdapter(LogicAdapter):
-
+ nlp = spacy.load("en_core_web_sm")
  def __init__(self, chatbot, **kwargs):
      super().__init__(chatbot, **kwargs)
 
  def can_process(self, statement):
-     """
-     Return true if the input statement contains
-     'what' and 'is' and 'temperature'.
-     """
-     words = ['How', 'is', 'the','weather','in','London?']
-     if all(x in statement.text.split() for x in words):
+     user_input = statement.text.lower()
+     if 'weather' in user_input:
          return True
      else:
          return False
 
  def process(self, input_statement, additional_response_selection_parameters):
      from chatterbot.conversation import Statement
-     import requests
-
+     doc = self.nlp(input_statement.text.lower())
      # Make a request to the temperature API
      # api-endpoint
+     for ent in doc.ents:
+         # Print the entity text and its label
+         print(ent.text, ent.label_)
+         if(ent.label_=="GPE"):
+             data=self.find_weather(ent.text)
+             # Let's base the confidence value on if the request was successful
+
+             response_statement = Statement(data)
+             response_statement.confidence=1
+             return response_statement
+     response_statement = Statement("No GPE found to query weather Data")
+     response_statement.confidence = 1
+     return response_statement
+ @staticmethod
+ def find_weather(city):
+     from chatterbot.conversation import Statement
+     import requests
      URL = "https://community-open-weather-map.p.rapidapi.com/weather"
 
      # location given here
 
-
      # defining a params dict for the parameters to be sent to the API
-     PARAMS = {"q": "London,uk"}
+     PARAMS = {"q": city}
 
      HEADERS = {"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-	"x-rapidapi-key": "2b57e58d9cmsh9b17b2536cbf8c6p12acc0jsna8e3b52625cb",
-	"useQueryString": "true"}
+                "x-rapidapi-key": "2b57e58d9cmsh9b17b2536cbf8c6p12acc0jsna8e3b52625cb",
+                "useQueryString": "true"}
 
      # sending get request and saving the response as response object
-     response = requests.get(url=URL,headers=HEADERS, params=PARAMS)
+     response = requests.get(url=URL, headers=HEADERS, params=PARAMS)
      print(response.json())
-     data=response.json()
-     # Let's base the confidence value on if the request was successful
-     if response.status_code == 200:
-         confidence = 1
-     else:
-         confidence = 0
-
-
-
-     response_statement = Statement(data)
-     response_statement.confidence = confidence
-     return response_statement
+     data = response.json()
+     return data
